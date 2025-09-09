45f7e95dd9ab0860f4207a64ce4b0c90c1c6da6e
pb-06 <kkandomail@gmail.com>
2025-09-09 12:29:04 +0200

    Solve task 02

--- modules/person.js
+++ modules/person.js
@@ -1,5 +1,12 @@
 export default class Person {
     _name;
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+
     constructor(name) {
         this.name = name;
     }
